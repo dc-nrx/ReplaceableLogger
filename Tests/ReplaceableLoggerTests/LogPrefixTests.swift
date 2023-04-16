237593d35b2144ab8709d71425be4378474819e5
Dmytro Chapovskyi <dchapovskyi@shutterstock.com>
2023-04-16 11:54:23 +0200

    log prefix tests implemented

--- Tests/ReplaceableLoggerTests/LogPrefixTests.swift
+++ Tests/ReplaceableLoggerTests/LogPrefixTests.swift
@@ -19,7 +19,7 @@ final class LogPrefixTests: XCTestCase {
 		}
     }
 
-	func testLevelPrefixes_addedToProperLevels() throws {
+	func testLevelPrefixes_addedToCorrespondingLevels() throws {
 		let levelPrefixes = LogLevel.sampleLevelPrefixesDict
 		let sut = DefaultLoggerMock(.verbose, levelPrefixes: levelPrefixes)
 		for level in LogLevel.allCases {
@@ -28,5 +28,42 @@ final class LogPrefixTests: XCTestCase {
 		}
 	}
 	
-	
+	func testSpacesAndOrder_between_commonPrefix_levelPrefix_andMessage() {
+		let levelPrefixes = LogLevel.sampleLevelPrefixesDict
+		let message = "message_without_spaces"
+		let commonPrefix = "zzz"
+		let sut = DefaultLoggerMock(.verbose,
+									levelPrefixes: levelPrefixes,
+									commonPrefix: commonPrefix)
+		for level in LogLevel.allCases {
+			sut.log(level, message)
+			let components = sut.lastMessage?.components(separatedBy: " ") ?? []
+			XCTAssertEqual(components.count, 3, "message: \(sut.lastMessage ?? "<nil>")")
+			XCTAssertEqual(components[0], commonPrefix)
+			XCTAssertEqual(components[1], levelPrefixes[level])
+			XCTAssertEqual(components[2], message)
+		}
+	}
+
+	func testSpaces_between_commonPrefix_andMessage_noLevelPrefixes() {
+		let sut = DefaultLoggerMock(.verbose,
+									levelPrefixes: nil,
+									commonPrefix: "zzz")
+		for level in LogLevel.allCases {
+			sut.log(level, "message_without_spaces")
+			let components = sut.lastMessage?.components(separatedBy: " ") ?? []
+			XCTAssertEqual(components.count, 2, "message: \(sut.lastMessage ?? "<nil>")")
+		}
+	}
+
+	func testSpaces_between_levelPrefixes_andMessage_noCommonPrefix() {
+		let sut = DefaultLoggerMock(.verbose,
+									levelPrefixes: nil,
+									commonPrefix: "zzz")
+		for level in LogLevel.allCases {
+			sut.log(level, "message_without_spaces")
+			let components = sut.lastMessage?.components(separatedBy: " ") ?? []
+			XCTAssertEqual(components.count, 2, "message: \(sut.lastMessage ?? "<nil>")")
+		}
+	}
 }
